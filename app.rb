450b49411cfd015ebe8ecb939590c7a136d27a47
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-25 17:46:41 +0100

    named cat route with params

--- app.rb
+++ app.rb
@@ -9,7 +9,14 @@ get '/secret' do
   "Shhh"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
